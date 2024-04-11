c8c3027c424b2699d2a6503d37c239a511c391de
Stefan Koev <st.koev@outlook.com>
2024-04-11 17:52:34 +0300

    finish custommer reviews section

--- src/sections/CustommerReviews.jsx
+++ src/sections/CustommerReviews.jsx
@@ -1,5 +1,29 @@
-const CustommerReviews = (props) => {
-  return <div></div>;
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+const CustommerReviews = () => {
+  return (
+    <section className="max-container">
+      <h3 className="font-palaquin text-center text-4xl font-bold">
+        What Our?
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied custommers.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14 ">
+        {reviews.map((rev) => (
+          <ReviewCard
+            key={rev.customerName}
+            imgURL={rev.imgURL}
+            customerName={rev.customerName}
+            rating={rev.rating}
+            feedback={rev.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustommerReviews;
