8c887546f27b4e28c078d36cedd458889519ebd7
Stefan Koev <st.koev@outlook.com>
2024-04-11 21:11:22 +0300

    fix special offer section bug, final commit

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -4,7 +4,7 @@ import { arrowRight } from "../assets/icons";
 
 const SpecialOffer = () => {
   return (
-    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 maxcontainer">
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
       <div className="flex-1">
         <img
           src={offer}
