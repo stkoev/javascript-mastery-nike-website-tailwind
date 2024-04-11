d03895e084df70c9aadd175e7cd9542e33e6eff9
Stefan Koev <st.koev@outlook.com>
2024-04-11 15:53:53 +0300

    finish servicecard and services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
-const Services = (props) => {
-  return <div></div>;
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
+const Services = () => {
+  // imgURL, label, subtext
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((srv) => (
+        <ServiceCard key={srv.label} {...srv} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
